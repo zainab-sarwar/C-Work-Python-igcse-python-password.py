59a458eff7fe19a8e9c69e2ee325cd87ab7c977f
zainab-sarwar <112849040+zainab-sarwar@users.noreply.github.com>
2022-09-14 11:32:30 +0400

    Add files via upload

---
+++ revision.py
@@ -0,0 +1,14 @@
+x=int(input("Enter a number."))
+y=int(input("Enter a second number."))
+z=int
+a=int
+b=int
+
+if x>y:
+    z=2*x*y-3
+    print (z)
+elif x==y:
+    a=(x*y)-(x+y)
+    print (a)
+else:
+    b=24*x*y
